140b699654b3a79b8ea6f70c7f30785be46f8672
EsleySaab <esleysantana.sa@gmail.com>
2025-02-12 21:36:52 -0300

    feat: creating Song page

--- src/pages/Song-page.jsx
+++ src/pages/Song-page.jsx
@@ -1,4 +1,33 @@
-export const SongPage = () => {
-  return <h1>Song</h1>
-}
+import { Link } from "react-router-dom"
+import { Player } from "../components/Player"
 
+export const SongPage = () => {
+  return (
+    <div className="song">
+      <div className="song__container">
+        <div className="song__image-container">
+          <img
+            src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
+            alt="Imagem da música X"
+          />
+        </div>
+      </div>
+
+      <div className="song__bar">
+        <Link to="/artist/3" className="song__artist-image">
+          <img
+            width={75}
+            height={75}
+            src="https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9"
+            alt="Imagem do artista Y"
+          />
+        </Link>
+        <Player />
+        <div>
+          <p className="song__name">Última Saudade - Ao Vivo</p>
+          <p>Henrique & Juliano</p>
+        </div>
+      </div>
+    </div>
+  )
+}
